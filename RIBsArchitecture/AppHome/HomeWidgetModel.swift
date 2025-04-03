52e916ced3a313b4d1ce1f19e6a3c59846cc913b
RAFA <rafa71802937@gmail.com>
2025-04-03 17:38:39 +0900

    fear: HomeWidgetModel 구현
    이미지 이름, 제목, 탭 핸들러를 포함하여 위젯의 기본 구조를 제공

---
+++ RIBsArchitecture/AppHome/HomeWidgetModel.swift
@@ -0,0 +1,14 @@
+//
+//  HomeWidgetModel.swift
+//  RIBsArchitecture
+//
+//  Created by RAFA on 4/3/25.
+//
+
+import Foundation
+
+struct HomeWidgetModel {
+    let imageName: String
+    let title: String
+    let tapHandler: () -> Void
+}
