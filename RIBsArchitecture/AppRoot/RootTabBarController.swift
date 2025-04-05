89f5039290aa24d027e77f63892bbda4e5a74670
RAFA <rafa71802937@gmail.com>
2025-04-05 20:36:05 +0900

    feat: AppRoot 모듈 추가 및 탭바 컨트롤러 구현
    AppRootBuilder, AppRootInteractor, AppRootRouter, RootTabBarController 파일을 추가하여
    앱의 루트 구조를 구성함. 각 RIB 간의 의존성을 설정하고 탭바 컨트롤러를 통해
    다양한 홈 화면으로 전환할 수 있도록 구현하였음.

---
+++ RIBsArchitecture/AppRoot/RootTabBarController.swift
@@ -0,0 +1,31 @@
+//
+//  RootTabBarController.swift
+//  RIBsArchitecture
+//
+//  Created by RAFA on 4/5/25.
+//
+
+import UIKit
+
+import ModernRIBs
+
+protocol AppRootPresentableListener: AnyObject {
+
+}
+
+final class RootTabBarController: UITabBarController, AppRootViewControllable, AppRootPresentable {
+
+    weak var listener: AppRootPresentableListener?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tabBar.isTranslucent = false
+        tabBar.tintColor = .black
+        tabBar.backgroundColor = .white
+    }
+
+    func setViewControllers(_ viewControllers: [ViewControllable]) {
+        super.setViewControllers(viewControllers.map(\.uiviewController), animated: false)
+    }
+}
