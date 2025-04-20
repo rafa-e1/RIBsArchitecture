ac667b78103fa3342b9d587f08b83605660de631
RAFA <rafa71802937@gmail.com>
2025-04-20 19:40:58 +0900

    feat: FinanceHomeRouter에 cardOnFileDashboardBuilder 추가
    FinanceHomeRouter 생성 시 cardOnFileDashboardBuilder를 추가하여
    카드 파일 대시보드 기능을 연동할 수 있도록 수정함

--- RIBsArchitecture/FinanceHome/FinanceHomeBuilder.swift
+++ RIBsArchitecture/FinanceHome/FinanceHomeBuilder.swift
@@ -51,7 +51,8 @@ final class FinanceHomeBuilder: Builder<FinanceHomeDependency>, FinanceHomeBuild
         return FinanceHomeRouter(
             interactor: interactor,
             viewController: viewController,
-            superPayDashboardBuildable: superPayDashboardBuilder
+            superPayDashboardBuildable: superPayDashboardBuilder,
+            cardOnFileDashboardBuildable: cardOnFileDashboardBuilder
         )
     }
 }
