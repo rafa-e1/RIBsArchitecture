27a854db5cc26d4028bc4291751de9841c299c30
RAFA <rafa71802937@gmail.com>
2025-04-20 19:53:33 +0900

    feat: 카드 및 계좌 대시보드 화면 추가
    CardOnFileDashboardViewController.swift 파일을 생성하여
    카드 및 계좌 정보를 보여주는 대시보드 화면을 구현함
    
    - UIStackView를 사용하여 헤더와 카드 목록 영역 구성
    - "전체보기" 버튼과 결제 수단 추가 버튼 배치
    - 기본적인 레이아웃 제약 조건 설정 완료

---
+++ RIBsArchitecture/FinanceHome/CardOnFileDashboard/CardOnFileDashboardViewController.swift
@@ -0,0 +1,108 @@
+//
+//  CardOnFileDashboardViewController.swift
+//  RIBsArchitecture
+//
+//  Created by RAFA on 4/19/25.
+//
+
+import ModernRIBs
+import UIKit
+
+protocol CardOnFileDashboardPresentableListener: AnyObject {
+    // TODO: Declare properties and methods that the view controller can invoke to perform
+    // business logic, such as signIn(). This protocol is implemented by the corresponding
+    // interactor class.
+}
+
+final class CardOnFileDashboardViewController: UIViewController, CardOnFileDashboardPresentable, CardOnFileDashboardViewControllable {
+
+    weak var listener: CardOnFileDashboardPresentableListener?
+
+    private let headerStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .equalSpacing
+        return stackView
+    }()
+
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "카드 및 계좌"
+        label.font = .systemFont(ofSize: 22, weight: .semibold)
+        return label
+    }()
+
+    private lazy var seeAllButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("전체보기", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.addTarget(self, action: #selector(seeAllButtonDidTap), for: .touchUpInside)
+        return button
+    }()
+
+    private let cardOnFileStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 12
+        return stackView
+    }()
+
+    private lazy var addMethodButton: AddPaymentMethodButton = {
+        let button = AddPaymentMethodButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.roundCorners()
+        button.backgroundColor = .systemGray4
+        button.addTarget(self, action: #selector(addButtonDidTap), for: .touchUpInside)
+        return button
+    }()
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+
+        setupViews()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+
+        setupViews()
+    }
+
+    private func setupViews() {
+        view.addSubview(headerStackView)
+        view.addSubview(cardOnFileStackView)
+
+        headerStackView.addArrangedSubview(titleLabel)
+        headerStackView.addArrangedSubview(seeAllButton)
+
+        cardOnFileStackView.addArrangedSubview(addMethodButton)
+
+        NSLayoutConstraint.activate([
+            headerStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 10),
+            headerStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            headerStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+
+            cardOnFileStackView.topAnchor.constraint(equalTo: headerStackView.bottomAnchor, constant: 10),
+            cardOnFileStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            cardOnFileStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            cardOnFileStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+
+            addMethodButton.heightAnchor.constraint(equalToConstant: 60)
+        ])
+    }
+
+    @objc private func seeAllButtonDidTap() {
+
+    }
+
+    @objc private func addButtonDidTap() {
+
+    }
+}
